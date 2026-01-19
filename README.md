854dfc63ba04a6266e94f4f6c0a1aa9170250fa0
gilbett-f <gilbertferdinandalexantio@gmail.com>
2026-01-19 17:17:38 +0700

    h-1 before final i hope

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# amba-cafe
-a simple cafe website
+# amba cafe
+amba cafe website is a simple fnb website made with html-css-js. for amba cafe's website, developer prioritize simplicity but remains modern.
--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-console.log("jav4 script Work");
- 
- // Mobile menu toggle
+// Mobile menu toggle
  const hamburger = document.querySelector('.hamburger');
  const navMenu = document.querySelector('.nav-menu');
  hamburger.addEventListener('click', () => {
-     alert("clicked")
      hamburger.classList.toggle('active');
      navMenu.classList.toggle('active');
  });
-
-console.log(hamburger);
-console.log(navMenu);
 
  // close mobile menu when clicking on a link
  document.querySelectorAll('.nav-link').forEach(link => {
